655180aaf9bce5f59eec5415c28351a71957a1e6
spencerk226 <spencerk226@gmail.com>
2020-11-12 20:17:05 -0600

    Add 'recordings' page to website

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import notifacation
 import util
 import datetime
 import json
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, render_template_string, Response
 import time
 import os
 import psutil
@@ -85,12 +85,15 @@ def motionDetected():
         timeInfo = datetime.datetime.now()
         recName = timeInfo.strftime(settings["fileNameFormat"])
         #might add check to see if file already exists, then add something so that stuff doesn't get overwritten
-        out = cv2.VideoWriter(settings["videoOut"] + recName + ".mp4",cv2.VideoWriter_fourcc(*"mp4v"), settings["outfileFramerate"], (640,480))
+        out = cv2.VideoWriter(settings["videoOut"] + recName + ".webm",cv2.VideoWriter_fourcc(*"VP80"), settings["outfileFramerate"], (640,480))#out = cv2.VideoWriter(settings["videoOut"] + recName + ".mp4",cv2.VideoWriter_fourcc(*"mp4v"), settings["outfileFramerate"], (640,480))
     else: 
         if active and armed:
             resetTimer()
             setTimer()
             timer.start()
+
+def get_performance():
+    return "cpu: " + str(psutil.cpu_percent()) + " ram: " + str(psutil.virtual_memory().percent)
 
 def main():
     global frame
@@ -98,6 +101,7 @@ def main():
 
     while 1:
         ret, frame = cap.read()
+        #frame = cv2.resize( frame, (640,480))
         motionFrame = getMotion( frame)
         motionDetectedBool = percentWhite( frame, motionFrame) > 4.0
     
@@ -106,14 +110,16 @@ def main():
             motionFrame = cv2.putText( motionFrame, "Motion Detected", (10,22), cv2.FONT_HERSHEY_SIMPLEX, .7, (100,100,100), 1)
 
 
+        timeInfo = datetime.datetime.now()
+        recName = timeInfo.strftime("%a %d/%m/%Y %I:%M:%S %Z")
+        frame = cv2.putText( frame, recName, (10,470), cv2.FONT_HERSHEY_SIMPLEX, .4, (250,250,250), 1)
+        cv2.putText( frame, get_performance(), (10,15), cv2.FONT_HERSHEY_SIMPLEX, .4, (250,250,250), 1)
+
         if showImages:
             cv2.imshow("Frame", frame)
             cv2.imshow("Motion", motionFrame)
 
         if active:
-            timeInfo = datetime.datetime.now()
-            recName = timeInfo.strftime("%a %d/%m/%Y %I:%M:%S %Z")
-            frame = cv2.putText( frame, recName, (10,470), cv2.FONT_HERSHEY_SIMPLEX, .4, (250,250,250), 1)
             out.write(frame)
         #os.system(settings["clearCommand"])
         #print("cpu: " + str(psutil.cpu_percent()) + " ram: " + str(psutil.virtual_memory().percent))
@@ -131,7 +137,7 @@ def main():
             break     
         time.sleep(settings["loopDelay"])
 
-app=Flask(__name__)
+app=Flask(__name__,static_folder='Recordings')
 
 @app.route('/')
 def home():
@@ -139,15 +145,59 @@ def home():
 
 @app.route('/video_feed')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/motion_view')
 def motion_view():
-    """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen_frames_motion(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/video_records')
+def return_videos():
+    ret = """
+    <!DOCTYPE html>
+    <html>
+    <body>
+    """
+
+    video_str = """
+    <p>{vanity}</p>
+    <video height="480" width="640" controls>
+        <source src=\"{lbrace}{lbrace} url_for('static', filename=\"{filename}\") {rbrace}{rbrace}\">
+    </video>
+    <br>
+    """
+    for filename in reversed(os.listdir("./Recordings/")):
+        if filename.endswith(".mp4") or filename.endswith(".webm"):
+            ret = ret + video_str.format(filename = filename, lbrace = "{", rbrace="}", vanity = filename.replace(".webm", ""))
+
+    ret += """
+    </html>
+    </body>
+    """
+    return render_template_string(ret)
+
+@app.route("/test")
+def test():
+    ret = """
+    <!DOCTYPE html>
+    <html>
+    <body>
+    """
+
+    video_str = """
+    <video height="480" width="640" controls>
+        <source src=\"{lbrace}{lbrace} url_for('static', filename='{filename}') {rbrace}{rbrace}\" type=\"video/mp4\">
+    </video>
+    <br>
+    """
+    ret += video_str
+    ret += """
+    </html>
+    </body>
+    """
+    return render_template_string( ret.format(filename = "rick.mp4", lbrace = "{", rbrace = "}"))
 def startApp():
     app.run(host='0.0.0.0')
 
@@ -179,9 +229,10 @@ def gen_frames_motion():
 
         timeInfo = datetime.datetime.now()
         recName = timeInfo.strftime("%a %d/%m/%Y %I:%M:%S %Z")
-        motionFrame = cv2.putText( motionFrame, recName, (10,470), cv2.FONT_HERSHEY_SIMPLEX, .4, (250,250,250), 1)
+        #motionFrame = cv2.putText( motionFrame, recName, (10,470), cv2.FONT_HERSHEY_SIMPLEX, .4, (250,250,250), 1)
+        #outFrame = cv2.putText( motionFrame, recName, (10,470), cv2.FONT_HERSHEY_SIMPLEX, .4, (250,250,250), 1)
 
-        ret, buffer = cv2.imencode('.jpg', motionFrame)
+        ret, buffer = cv2.imencode('.jpg', cv2.putText( motionFrame, recName, (10,470), cv2.FONT_HERSHEY_SIMPLEX, .4, (250,250,250), 1))
         out = buffer.tobytes()
         yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + out + b'\r\n')  # concat frame one by one and show result
