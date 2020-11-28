ac4ec2039200cbc9feac5810208b2c66a0170a0b
spencerk226 <spencerk226@gmail.com>
2020-11-28 17:04:04 -0600

    Add login page to make it secure-ER (bad)

--- init.py
+++ init.py
@@ -1,6 +1,6 @@
 # I want to run the settingsInit.py and any maintenence UI stuff here, while main handles starting this up and running camera.
 # Need to add making sure video directory exists.
-from util import getSettings
+from util import getSettings, install
 import initSettings
 
 import os
@@ -12,3 +12,4 @@ def run():
     print("Making output file")
     if not os.path.exists(settings["videoOut"]):
         os.mkdir(settings["videoOut"])
+    install("pyopenssl")
